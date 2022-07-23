80f2a31d6121e82647a77aee9c3b57c81852c3be
Decker Urbano <decker.urbano@geopagos.com>
2022-07-23 14:01:29 -0300

    chore: add linter

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
-import Head from 'next/head'
-import Image from 'next/image'
+import Head from 'next/head';
+import Image from 'next/image';
 
-import styles from '@/pages/index.module.css'
+import styles from '@/pages/index.module.css';
 
 export default function Home() {
   return (
@@ -31,19 +31,14 @@ export default function Home() {
             <p>Learn about Next.js in an interactive course with quizzes!</p>
           </a>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
+          <a href="https://github.com/vercel/next.js/tree/canary/examples" className={styles.card}>
             <h3>Examples &rarr;</h3>
             <p>Discover and deploy boilerplate example Next.js projects.</p>
           </a>
 
           <a href="https://vercel.com/new" className={styles.card}>
             <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
+            <p>Instantly deploy your Next.js site to a public URL with Vercel.</p>
           </a>
         </div>
       </main>
@@ -61,5 +56,5 @@ export default function Home() {
         </a>
       </footer>
     </div>
-  )
+  );
 }
