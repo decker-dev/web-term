217f52d98f1c29ed2993f16108b38da3c9f0fac0
Decker Urbano <decker.urbano@geopagos.com>
2022-07-23 23:56:10 -0300

    feat: add clear component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,11 +4,14 @@ interface PageProps {
   inputRef: React.MutableRefObject<HTMLInputElement>;
 }
 export const App: React.FC<PageProps> = ({ inputRef }) => {
-  const [history, setHistory] = useState<Array<string>>([]);
+  const [history, setHistory] = useState<Array<string>>(['Welcome to the CLI']);
 
   const HandleKeyUp = (e: React.KeyboardEvent<HTMLInputElement>) => {
     if (e.key === 'Enter' || e.code === '13') {
       setHistory([...history, e.currentTarget.value]);
+      if (e.currentTarget.value == 'clear') {
+        setHistory([]);
+      }
       e.currentTarget.value = '';
     }
   };
